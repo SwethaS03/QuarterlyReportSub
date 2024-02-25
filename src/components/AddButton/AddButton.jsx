f2e25abad3f9db4ab0a6c01a8e1a0138df7e7c20
SwethaS03 <SwethaS03@users.noreply.github.com>
2024-02-25 10:52:37 +0530

    Button

---
+++ src/components/AddButton/AddButton.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+// import "./AddButton.css";
+
+const AddButton = () => {
+    const handleClick = () => {
+        // Handle login logic here
+    };
+
+    return (
+        <button onClick={handleClick} className="button">
+            Add
+        </button>
+    );
+};
+
+export default AddButton;
---
+++ src/pages/LoginPage.jsx
@@ -0,0 +1,12 @@
+import AddButton from "../components/AddButton/AddButton";
+
+//import "../index.css";
+function LoginPage() {
+  return (
+      <div className="login-page">
+        <AddButton />
+      </div>
+  );
+}
+
+export default LoginPage;
