23bfc89f2cda894ad1f2ab0170af135a5986d084
SwethaS03 <SwethaS03@users.noreply.github.com>
2024-02-26 22:31:40 +0530

    QuaterRep Updated

---
+++ src/pages/QuarterRepPage.jsx
@@ -0,0 +1,14 @@
+import QuarterRep from "../components/QuarterRep/QuarterRep";
+
+//import "../index.css";
+function QuarterRepPage() {
+  return (
+
+      <div className="quarter-rep">
+        <QuarterRep />
+
+      </div>
+  );
+}
+
+export default QuarterRepPage;
