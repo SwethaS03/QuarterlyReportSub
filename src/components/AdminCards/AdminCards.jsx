4ed137d1814d641ac4c38f6c11b865cde575f462
SwethaS03 <SwethaS03@users.noreply.github.com>
2024-02-26 17:00:50 +0530

    AdminCard

--- src/components/AdminCards/AdminCards.jsx
+++ src/components/AdminCards/AdminCards.jsx
@@ -1,21 +1,53 @@
 import React from 'react';
 import "./AdminCards.css";
-import addUserIcon from "../../images/addUser.svg";
+import AdminCardData from "../../constants/AdminCardConstants.json";
+import { useNavigate } from "react-router-dom";
 
 const AdminCards = () => {
+    // const navigate = useNavigate() 
+    // const handleClick = (navigateTo) => {
+    //     // Handle login logic here
+    //     navigate(navigateTo)
+    // };
 
-    const handleClick = () => {
-        // Handle login logic here
-    };
-
+    // {AdminCardData.map((cardData, index) => {
+    //     return(
+    //         <div key={index} className="card" onClick={() => {
+    //             // Handle login logic here
+    //             // navigate(cardData.navigate)
+    //         }}>
+    //            <img src= {cardData.ImageURL} alt="AdminCards" className="card-image" />
+    //            <h2 className="card-heading">{cardData.title}</h2>
+    //            <hr className="card-line" />
+    //            <p className="card-description">{cardData.description}</p>
+    //        </div>
+    //     );
+    // })}
     return (
-        <div className="card" onClick={handleClick}>
-            <img src= {addUserIcon} alt="AdminCards" className="card-image" />
-            <h2 className="card-heading">Add Department</h2>
-            <hr className="card-line" />
-            <p className="card-description">Add more departments to the existing list of departments.</p>
+        <div className="cards-container">
+            {AdminCardData.map((cardData, index) => {
+                return(
+                    <div key={index} className="card" >
+                    <img src= {cardData.ImageURL} alt="AdminCards" className="card-image" />
+                    <h2 className="card-heading">{cardData.title}</h2>
+                    <hr className="card-line" />
+                    <p className="card-description">{cardData.description}</p>
+                </div>
+                );
+            })}
         </div>
-    );
+        )
+      
+
+    
+    // return (
+    //     <div className="card">
+    //         <img src= {addUserIcon} alt="AdminCards" className="card-image" />
+    //         <h2 className="card-heading">Add Department</h2>
+    //         <hr className="card-line" />
+    //         <p className="card-description">Add more departments to the existing list of departments.</p>
+    //     </div>
+    // );
 };
 
 export default AdminCards;
