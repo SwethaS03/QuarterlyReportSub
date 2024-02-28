d98e62977055514eab7ec7b06cf3b1c0c5b06294
MrRhuezzler <anonymouspyro369@gmail.com>
2024-02-28 20:55:54 +0530

    updated-routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,26 @@
 import "./App.css";
+import AddDepartmentPage from "./pages/AddDepartmentPage";
+import AdminPage from "./pages/AdminPage";
+import DownloadPage from "./pages/DownloadPage";
 import LoginPage from "./pages/LoginPage";
-import { Route, Routes } from "react-router-dom";
+import { Navigate, Route, Routes } from "react-router-dom";
+import QuarterAdminPage from "./pages/QuarterAdminPage";
+import QuarterRepPage from "./pages/QuarterRepPage";
 
 function App() {
   return (
     <Routes>
-      <Route path="/login" element={<LoginPage />} />
+      <Route path="/">
+        <Route index element={<Navigate to="/login" />} />
+        <Route path="selector" element={<QuarterRepPage />} />
+      </Route>
+      <Route path="/admin">
+        <Route path="" element={<AddDepartmentPage />} />
+        <Route path="modify-order" /*Modify Order Page */ />
+        <Route path="add-department" element={<AddDepartmentPage />} />
+        <Route path="selector" element={<AddDepartmentPage />} />
+        <Route path="selector/:year/:quarter" element={<DownloadPage />} />
+      </Route>
     </Routes>
   );
 }
