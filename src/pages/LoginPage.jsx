a4c065f3f97f8f5041d930ab3174afd4c815c425
SwethaS03 <SwethaS03@users.noreply.github.com>
2024-02-28 22:07:21 +0530

    LoginPage updated

--- src/pages/LoginPage.jsx
+++ src/pages/LoginPage.jsx
@@ -1,18 +1,32 @@
-// import LoginButton from "../components/LoginButton/LoginButton";
-import NavbarLogout from "../components/NavbarLogout/NavbarLogout";
+import LoginCard from "../components/LoginCard/LoginCard";
+import React from "react";
+import psg from "/images/psg-logo-name.svg";
+import background from "/images/vector.svg";
 
-//import "../index.css";
+import "./LoginPage.css"
+
 function LoginPage() {
   return (
+    <div className='default-bg'>
+      <img className='psg-image' src={psg}></img>
+      <div className="parent-container">
 
-      <div className="login-page">
-        {/* <LoginButton /> */}
-        <NavbarLogout />
+        <div className="left-cotainer">
 
-
-
+          <div className='img-container'>
+            <LoginCard />
+          </div>
+        </div>
+        <div className="right-cotainer">
+          <img className='vector-img' src={background}></img>
+        </div>
       </div>
+    </div>
   );
-}
+};
+
+
+
+
 
 export default LoginPage;
