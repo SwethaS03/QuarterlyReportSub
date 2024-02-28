9a7b964254f5ab60d953391a9ab84e8073c1960a
SwethaS03 <SwethaS03@users.noreply.github.com>
2024-02-28 23:13:05 +0530

    AdminPage updated

--- src/components/AdminCards/AdminCards.jsx
+++ src/components/AdminCards/AdminCards.jsx
@@ -4,25 +4,7 @@ import AdminCardData from "../../constants/AdminCardConstants.json";
 import { useNavigate } from "react-router-dom";
 
 const AdminCards = () => {
-    // const navigate = useNavigate() 
-    // const handleClick = (navigateTo) => {
-    //     // Handle login logic here
-    //     navigate(navigateTo)
-    // };
 
-    // {AdminCardData.map((cardData, index) => {
-    //     return(
-    //         <div key={index} className="card" onClick={() => {
-    //             // Handle login logic here
-    //             // navigate(cardData.navigate)
-    //         }}>
-    //            <img src= {cardData.ImageURL} alt="AdminCards" className="card-image" />
-    //            <h2 className="card-heading">{cardData.title}</h2>
-    //            <hr className="card-line" />
-    //            <p className="card-description">{cardData.description}</p>
-    //        </div>
-    //     );
-    // })}
     return (
         <div className="cards-container">
             {AdminCardData.map((cardData, index) => {
@@ -37,17 +19,7 @@ const AdminCards = () => {
             })}
         </div>
         )
-      
 
-    
-    // return (
-    //     <div className="card">
-    //         <img src= {addUserIcon} alt="AdminCards" className="card-image" />
-    //         <h2 className="card-heading">Add Department</h2>
-    //         <hr className="card-line" />
-    //         <p className="card-description">Add more departments to the existing list of departments.</p>
-    //     </div>
-    // );
 };
 
 export default AdminCards;
--- src/pages/AdminPage.jsx
+++ src/pages/AdminPage.jsx
@@ -1,14 +1,24 @@
 import AdminCards from "../components/AdminCards/AdminCards";
-
+import NavbarLogout from "../components/NavbarLogout/NavbarLogout";
+import "./AdminPage.css"
 //import "../index.css";
 
 function AdminPage() {
   return (
-
-      <div className="admin-cards">
-        <AdminCards />
-
+      <div className='default-bg'>
+        <NavbarLogout/>
+          <div className='text-card-container'>
+            <div className='text-container'>
+              <h2>Admin</h2>
+              <hr className="card-line" />
+            </div>
+            <div className="admin-cards">
+            <AdminCards />
+            </div>
+          </div>
+          
       </div>
+      
   );
 }
 
