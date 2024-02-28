67d344b58136a847f9d20d729ad52e1fd3cca776
J-Akshaya02 <20pw03@psgtech.ac.in>
2024-02-28 16:13:18 +0530

    download page

--- src/pages/DownloadPage.jsx
+++ src/pages/DownloadPage.jsx
@@ -1,11 +1,29 @@
 import DownloadButton from "../components/DownloadButton/DownloadButton";
+import Navbar from "../components/NavbarBack&Logout/NavbarBack&Logout";
+import DownloadCard from "../components/DownloadCard/DownloadCard";
+import "./DownloadPage.css";
 
 function DownloadPage() {
     return (
-  
-        <div className="download-button">
-          <DownloadButton/>
+      <div className="default-bg">
+        <Navbar />
+        <div className="container">
+          <div className="title-container">
+            <h3>ADMIN</h3>
+            <h2>Download</h2>
+          </div>
+          <div className="download">
+            <DownloadCard />
+            <DownloadCard />
+            <DownloadCard />
+            <DownloadCard />
+          </div>
+          <div className="button-container">
+            <DownloadButton/>
+          </div>
         </div>
+          
+      </div>
     );
   }
   
