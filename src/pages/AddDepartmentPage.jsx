92f9b8d8997fb50bb107ed8296dc792eceb02c96
SwethaS03 <SwethaS03@users.noreply.github.com>
2024-02-28 16:09:08 +0530

    Add Dept Page

--- src/pages/AddDepartmentPage.jsx
+++ src/pages/AddDepartmentPage.jsx
@@ -1,12 +1,32 @@
 import AddButton from "../components/AddButton/AddButton";
+import AddDeptCard from "../components/AddDeptCard/AddDeptCard";
+import NavbarBackAndLogout from "../components/NavbarBack&Logout/NavbarBack&Logout";
+import ExistingUsersCard from "../components/ExistingUsersCard/ExistingUsersCard";
+import React from "react";
+import "./AddDepartmentPage.css"
 
 function AddDepartmentPage() {
-    return (
-  
-        <div className="add-button">
-          <AddButton/>
+  return (
+    <div className="default-bg">
+      <NavbarBackAndLogout />
+      <div className="parent-container">
+
+        <div className="left-cotainer">
+
+          <div className="title-container">
+            <h3>ADMIN</h3>
+            <h1>Add Department</h1>
+          </div>
+          <div className='add-dept'>
+            <AddDeptCard />
+          </div>
         </div>
-    );
-  }
-  
-  export default AddDepartmentPage;
+        <div className="right-cotainer">
+          <ExistingUsersCard />
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default AddDepartmentPage;
